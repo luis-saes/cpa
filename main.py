1af1bf4d22e5743167c6cbfd2909f05a89f88974
ElMigu17 <rgomiguel17@gmail.com>
2021-09-18 17:47:18 -0300

    fiz o rankeiamento e a soma dele

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import pandas as pd
 import csv
 
+
+'''
+Friedman aligned-ranks test; 
+Friedman two-way analysis of variance by ranks; 
+Iman-Davenport test; 
+Quade test
+
+link da tabela com os role Alpha: https://www.medcalc.org/manual/chi-square-table.php
+'''
+
 class Planilha:
     def __init__(self) -> None:
         self.fileName = "cpa"
@@ -21,16 +31,13 @@ class Planilha:
 
             if row[0] == 'pFO;;e':
                 row.pop(0)
-            print(row)
             self.rows[index] = row[0].split(";")
             self.rows[index] = list(filter(None, self.rows[index]))
     
     def writeTempCsv(self):
         tempString = ""
 
-        print(self.rows)
         for row in self.rows:
-            print(row)
             for el in row:
                 tempString += f"{el};"
             tempString = tempString[:-1]
@@ -43,6 +50,9 @@ class Planilha:
         pass
     
     def build(self):
+
+        self.readCsv()
+        self.clearRows()
         rows = []
         fo0 = []
         fo = []
@@ -75,7 +85,6 @@ class Planilha:
                 i = 4
                 while i < 19:
                     row[i+1] = row[i+1].replace(",", ".")
-                    print(i, row[i], row[i+1])
                     output.append((int(row[i]), float(row[i+1])))
                     i = i+2
                 this_row.output = output
@@ -99,39 +108,89 @@ class Row:
         self.fon = fon
         self.output = output
 
-    def rankeia(self):
-        rank = [[]]
-        
-        for row in self.FO:
-            row_copy = sorted(row.copy())
-            last = -1
-            pos_first_last = -1
-            for index,cell in row_copy:
-                if cell == last:
-                    continue
-                    #\/
-                qtd_repeted = index - pos_first_last
-                # qtd_repeted
+    
                
-                pass
 
-    
+def filtra(row, choice):
 
+    value_to_analise = []
+    for output in row.output:
+        value_to_analise.append(output[choice])
+    return value_to_analise
     
+def gera_posicoes(array):
+
+    array_copy = sorted(array.copy())
+    curretn_value = array_copy[0]
+    pos_first_last = 0
+    tam = len(array_copy)
+    i = 0
+    saida = []
+    while i < tam:
+        if array_copy[i] == curretn_value:
+            i += 1
+            continue
+            #\/
+        qtd_repeted = i - pos_first_last
+        dividendo = 0
+        for j in range(pos_first_last, i):
+            dividendo += j + 1
+        saida.append((curretn_value, dividendo/ qtd_repeted))
+        curretn_value = array_copy[i]
+        pos_first_last = i
+        i += 1
+
+    qtd_repeted = i - pos_first_last
+    dividendo = 0
+    for j in range(pos_first_last, i):
+        dividendo += j + 1
+    saida.append((curretn_value, dividendo/ qtd_repeted))
+    return saida
+        # qtd_repeted
+
+#isso aqui retorna um arrai com a ordem de rankeamento que a linha que foi enserida aqui tinha
+def rankeia(row):
+    linha = filtra(row, 0)
+    tuplas_rank = gera_posicoes(linha)
+    array_com_rank = []
+    for value in row.output:
+        for tupla in tuplas_rank:
+            if value[0] == tupla[0]:
+                array_com_rank.append((tupla[1]))
+                break
+        else:
+            print ("ERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRROOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
+    return array_com_rank
+
+def soma_ranks(conjunto):
+    matrix_rank = []
+    for row in conjunto.rows:
+        matrix_rank.append(rankeia(row))
+
+    tam = len(matrix_rank[0])
+    soma_ranks_saida = [0] * tam
+    for c in range(tam):
+        for r in range(len(matrix_rank)):
+            soma_ranks_saida[c] += matrix_rank[r][c]
+    print("saida:",soma_ranks_saida)
+
+
+
+
 
 def main():
     planilha = Planilha()
-    planilha.readCsv()
-    planilha.clearRows()
-    planilha.writeTempCsv()
     conjuntos = planilha.build()
-    print("----------------------------------------------------------------------------")
     for conjunto in conjuntos:
-        print("jkadfjkag")
+        print("jkadfjkag", conjunto)
+        soma_ranks(conjunto)
+        
         for row in conjunto.rows:
-            print("linha", row.id, row.n, row.E, row.fon, row.output)
+            linha = rankeia(row)
         print(conjunto.FOzero, conjunto.FO, conjunto.output)
     pass
+
+
 
 if __name__ == "__main__":
     main()
